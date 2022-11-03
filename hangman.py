401db312aa5b9c1be04661e35c2743ef2bafab82
Czerwiec <czerrwiec@gmail.com>
2022-11-03 19:19:09 +0100

    added random from txt and txt files

--- hangman.py
+++ hangman.py
@@ -1,6 +1,11 @@
 import re
+import random
 
-word = "hangman"
+
+
+
+
+# word = "hangman"
 used_letters = []
 word_list = []
 tries = 5
@@ -22,47 +27,78 @@ def score_table():
     print(word_list)
     print()
 
-
-for x in word:
-    word_list.append("_")
-    
-
-
-
 while True:
-    score_table()
-    letter = input("Podaj literę: ").lower()
-    if re.search(r"^[a-złżźąęśćóń]$", letter) and letter not in used_letters:
-        used_letters.append(letter)
-    else:
-        if letter in used_letters:
-            print("Podana litera została już użyta.")
-            continue
-        else:
-            print()
-            print("Podaj literę od a-z.")
-    
-    found_indexes = find_indexes(letter)
-
-    if len(found_indexes) == 0:
-        print("Nie ma takiej litery w tym słowie.")
+    print()
+    print("1. Imiona męskie")
+    print("2. Imiona żeńskie")
+    print("3. Imiona mieszane")
+    print("4. Rzeczowniki")
+    try:
+        user_choice = int(input("Wybierz zestaw haseł: "))
+        if user_choice == 1:
+            word = random.choice(open("Python hangman\imiona meskie.txt", "r", encoding="utf8").readlines()).lower()
+            word = word.rstrip('\n')
+        if user_choice == 2:
+            word = random.choice(open("Python hangman\imiona zenskie.txt", "r", encoding="utf8").readlines()).lower()
+            word = word.rstrip('\n')      
+        if user_choice == 3:
+            word = random.choice(open("Python hangman\imiona mieszane.txt", "r", encoding="utf8").readlines()).lower()
+            word = word.rstrip('\n')
+        if user_choice == 4:
+            word = random.choice(open("Python hangman\rzeczowniki.txt", "r", encoding="utf8").readlines()).lower()
+            word = word.rstrip('\n')
+    except:
+        ValueError
         print()
-        tries -= 1
+        print("Wybierz od 1-4.")
+        continue
     
-    for x in found_indexes:
-        word_list[x] = letter
 
-    if "".join(word_list) == word:
-        print(f"Brawo! Słowo to {word.upper()}")
+    print(word)
 
-    if tries == 0:
-        print(f"Liczba prób: {tries}")
-        choice = input("Niestety przegrałeś, czy chcesz odkryć słowo? (t/n) ")
-        if choice == "t" or choice == "T":
-            print()
-            print(f"Słowo to {word.upper()}")
-            quit()
+    for x in word:
+        word_list.append("_")
+
+
+    while True:
+        score_table()
+        letter = input("Podaj literę: ").lower()
+        if re.search(r"^[a-złżźąęśćóń]$", letter) and letter not in used_letters:
+            used_letters.append(letter)
         else:
+            if letter in used_letters:
+                print("Podana litera została już użyta.")
+                continue
+            else:
+                print()
+                print("Podaj literę od a-z.")
+                continue
+        
+        found_indexes = find_indexes(letter)
+
+        if len(found_indexes) == 0:
+            print("Nie ma takiej litery w tym słowie.")
+            print()
+            tries -= 1
+        
+        for x in found_indexes:
+            word_list[x] = letter
+
+        if "".join(word_list) == word:
+            print(f"Brawo! Słowo to {word.upper()}")
+            print()
+            print(word_list)
+            print()  
             quit()
-            
+
+        if tries == 0:
+            print(f"Liczba prób: {tries}")
+            choice = input("Niestety przegrałeś, czy chcesz odkryć słowo? (t/n) ")
+            if choice == "t" or choice == "T":
+                print()
+                print(f"Słowo to {word.upper()}")
+                quit()
+            else:
+                quit()
+                
     
